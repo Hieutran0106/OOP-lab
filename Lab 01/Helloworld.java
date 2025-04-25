64f7585458406124bbff0b8bb6f266dd4b83a4df
Hieutran0106 <hieutrant0106@gmail.com>
2025-04-25 16:11:42 +0700

    Delete Lab 01 directory

--- Lab 01/Helloworld.java
+++
@@ -1,8 +0,0 @@
-
-package trenlop;
-public class Helloworld {
-    public static void main(String[] args) {
-        System.out.println("Xin chao cac ban!");
-         System.out.println("Hello world!");
-    }
-}
